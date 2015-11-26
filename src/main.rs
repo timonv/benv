5c8ccea2ca18d70d749d00cfcdf29b8ffe05273f
Timon Vonk <timonv@gmail.com>
2015-11-26 20:56:39 +0100

    Support multiple arguments in command line without wrapping in quotes

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ Benv
 A tool to load .env ('dotenv') files and set it as
 the environment for a program to run.
 
-Usage: benv <dotenv> <program>
+Usage: benv <dotenv> <program>...
        benv [--help | --version]
 
 Options:
@@ -36,7 +36,7 @@ Options:
 #[derive(Debug, RustcDecodable)]
 struct Args {
     arg_dotenv: String,
-    arg_program: String
+    arg_program: Vec<String>
 }
 
 #[cfg(not(test))]
@@ -45,20 +45,8 @@ fn main() {
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
 
-    if args.arg_dotenv == "" {
-      println!("Missing env file");
-      println!("{}", USAGE);
-      exit(1);
-    }
-
-    if args.arg_program == "" {
-      println!("Missing program");
-      println!("{}", USAGE);
-      exit(1);
-    }
-
     load_file(&Path::new(&args.arg_dotenv))
-      .and_then(|env_list| run(&args.arg_program, env_list))
+      .and_then(|env_list| run(&args.arg_program.join(" "), env_list))
       .and_then(|mut process| process.wait().map_err(BenvError::IO))
       .map_err(print_user_friendly_error_and_exit).unwrap();
 }
